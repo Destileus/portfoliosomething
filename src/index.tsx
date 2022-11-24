5b59f5134f673f5abe22cc36349470d7e9b9cde6
destileus <danielaraneva7@gmail.com>
2022-11-24 14:16:49 +0200

    react-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SomeExampleComponent from "./components/example/someExampleComponent";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
+<React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path='example' element={<SomeExampleComponent />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
