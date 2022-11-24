79d72d8f9151f2681f3422ecdc20cc8214a1cce5
destileus <danielaraneva7@gmail.com>
2022-11-24 10:33:47 +0200

    wijifijoijfjiskljklsfkj

---
+++ src/components/example/someExampleComponent.tsx
@@ -0,0 +1,16 @@
+import styles from './someExampleComponent.module.scss'
+import React from 'react'
+
+type SomeExampleComponentProps = {
+
+}
+
+const SomeExampleComponent: React.FC<SomeExampleComponentProps> = () => {
+  return (
+    <div>
+      SomeExampleComponent
+    </div>
+  )
+}
+
+export default SomeExampleComponent
