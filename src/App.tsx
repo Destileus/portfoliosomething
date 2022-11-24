a7dfc8e89ff64e135a158c87bb02807d20c0cc28
destileus <danielaraneva7@gmail.com>
2022-11-24 15:37:45 +0200

    another quick fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import ClickCount from './components/example/clickCount'
 import SomeExampleComponent from './components/example/someExampleComponent'
 import React from 'react'
-import Navbar from './components/example/navbar/navbar'
 
 const App: React.FC = () => {
   return (
